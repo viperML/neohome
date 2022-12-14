654bd8ba1019cdf2441fa4fdf176b8caebeb5c4d
Fernando Ayats <ayatsfer@gmail.com>
2022-12-14 22:55:27 +0000

    tweak giscus

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -41,13 +41,16 @@ const siteMetadata = {
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
     provider: 'giscus', // supported providers: giscus, utterances, disqus
+    // utterancesConfig: {
+    //   repo: "https://github.com/viperML/neohome"
+    // }
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+      repo: "viperML/neoinfra",
+      repositoryId: "R_kgDOIm7anQ",
+      category: "Announcements",
+      categoryId: "DIC_kwDOIm7anc4CTFKJ",
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
