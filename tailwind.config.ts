8e6ea49d9933760ea413ed5c81e6420d1587cf59
Fernando Ayats <fernando.ayats-llamas@inria.fr>
2024-06-10 16:04:47 +0200

    navbar stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      // spacing: {
+      //   '11': '4.75rem',
+      // }
+    },
   },
   plugins: [],
 } satisfies Config
