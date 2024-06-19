6422af2fe64b7b7b54517695df7239683180a198
Fernando Ayats <ayatsfer@gmail.com>
2024-06-19 14:59:28 +0200

    try dark theme switcher

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+  darkMode: 'selector',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
