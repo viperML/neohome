8eeef3c7358a5ead646fb850edc0f8ecfe782cd7
Fernando Ayats <ayatsfer@gmail.com>
2024-06-29 17:33:56 +0200

    tweaks

---
+++ public/scripts/theme.js
@@ -0,0 +1,25 @@
+const theme = (() => {
+    if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
+        return localStorage.getItem("theme");
+    }
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        return "dark";
+    }
+    return "light";
+})();
+
+if (theme === "light") {
+    document.documentElement.classList.remove("dark");
+} else {
+    document.documentElement.classList.add("dark");
+}
+
+window.localStorage.setItem("theme", theme);
+
+document.getElementById("themeToggle").addEventListener("click", () => {
+    const element = document.documentElement;
+    element.classList.toggle("dark");
+
+    const isDark = element.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+});
