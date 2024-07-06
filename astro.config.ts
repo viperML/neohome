8b90676ae9fac97d604f6c2e7cd026ba26ec4649
Fernando Ayats <ayatsfer@gmail.com>
2024-07-06 17:49:20 +0200

    cleanup

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,25 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind";
+import { remarkReadingTime } from './src/reading-time.mjs';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
     // your configuration options here...
     // https://docs.astro.build/en/reference/configuration-reference/
     integrations: [
-        tailwind()
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        mdx()
     ],
     devToolbar: {
         enabled: true
     },
     output: 'static',
     site: "https://ayats.org",
+    markdown: {
+        remarkPlugins: [
+            remarkReadingTime
+        ]
+    }
 })
