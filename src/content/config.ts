e0ec815da6346a7dd3c4893c725c5a1d4cd5508a
Fernando Ayats <ayatsfer@gmail.com>
2024-07-08 22:30:02 +0200

    add seo tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ import { z } from 'astro/zod';
 const blog = defineCollection({
   schema: rssSchema.extend({
     draft: z.boolean().optional(),
+    title: z.string(),
     summary: z.string(),
     pubDate: z.date(),
   }),
