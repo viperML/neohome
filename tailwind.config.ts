099fe674c09889959d4fd59effc442794b67bee2
Fernando Ayats <ayatsfer@gmail.com>
2024-07-14 11:24:12 +0200

    fix iosevka

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,8 @@
 import type { Config } from 'tailwindcss'
-const defaultTheme = require('tailwindcss/defaultTheme')
 
 export default {
   darkMode: 'selector',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'sans': ['"Geist Sans"', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   plugins: [],
 } satisfies Config
 
