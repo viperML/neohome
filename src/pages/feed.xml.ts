ca35cc8fe7f95faab850016413c722e75aa92947
Fernando Ayats <ayatsfer@gmail.com>
2024-09-19 17:58:22 +0200

    fix RSS feed?

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,48 +1,10 @@
-import type { APIContext } from 'astro';
-import sanitizeHtml from 'sanitize-html';
+import type { APIContext } from "astro";
 
-// import { experimental_AstroContainer as AstroContainer } from "astro/container";
-// import { getContainerRenderer as getMDXRenderer } from "@astrojs/mdx";
-// import { loadRenderers } from "astro:container";
-import { getCollection } from "astro:content";
-import rss from '@astrojs/rss';
-
-async function amap<A, B>(arr: A[], fun: (arg0: A) => B) {
-  return await Promise.all(arr.map(async v => await fun(v)))
-}
-
-export async function GET(context: APIContext): Promise<Response> {
-  const blog = (await getCollection('blog')).filter(post => {
-    return !(post.data.draft ?? false);
-  });
-
-  // https://github.com/withastro/roadmap/discussions/419
-
-  return rss({
-    title: 'ayats.org',
-    description: 'Welcome to my personal blog.',
-    site: (() => {
-      const s = context.site?.origin;
-      if (s !== undefined) {
-        return s;
-      } else {
-        throw new Error("Bad RSS site");
-      }
-    })(),
-    stylesheet: '/rss/pretty-feed-v3.xsl',
-    trailingSlash: false,
-    items: (await amap(blog, async (post) => {
-      // https://blog.damato.design/posts/astro-rss-mdx
-      // No idea about this
-      // const renderers = await loadRenderers([getMDXRenderer()]);
-      // const container = await AstroContainer.create({ renderers });
-      // const render = await post.render();
-      // container.renderToString(render.Content);
-      return {
-        link: `/blog/${post.slug}`,
-        // content: sanitizeHtml(await container.renderToString(render.Content)),
-        ...post.data,
-      };
-    })),
+export async function GET(context: APIContext) {
+  const endpoint = new URL('/feed', context.url.origin);
+  const xml = await fetch(endpoint).then((res) => res.text());
+  return new Response(xml, {
+    status: 200,
+    headers: new Headers({ 'Content-type': 'text/xml;charset=UTF-8' })
   });
 }
