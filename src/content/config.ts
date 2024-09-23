36070074e05a11f0e8087ae532be338b3a64d580
Fernando Ayats <ayatsfer@gmail.com>
2024-09-23 09:32:10 +0200

    remove rss properly

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection } from 'astro:content';
-import { rssSchema } from '@astrojs/rss';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-  schema: rssSchema.extend({
+  type: "content",
+  schema: z.object({
     draft: z.boolean().optional(),
     title: z.string(),
     summary: z.string(),
     pubDate: z.date(),
-  }),
-});
+  })
+})
 
 export const collections = { blog };
